a0a22ea092cf338ad8af7c8c4e08de6aed810f99
andrearm <andrearuizmendoza@gmail.com>
2013-11-04 22:42:30 -0500

    First activity over.

--- PrimeraActividad/primeraActividad.js
+++ PrimeraActividad/primeraActividad.js
@@ -15,46 +15,9 @@ Crear métodos de acceso y asignación (get y set) para las propiedades de la cl
 Puntaje adicional si se crea un solo método de acceso y otro de asignación.
 */
 
-User.prototype.setNombre = function(nombre) {
-	this.nombre = nombre;
-}
-User.prototype.getNombre = function() {
-	return this.nombre;
-}
+User.prototype.getPropiedad = function(attr) { return this[attr] };
 
-User.prototype.setApellidos = function(apellidos) {
-	this.apellidos = apellidos;
-}
-User.prototype.getApellidos = function() {
-	return this.apellidos;
-}
-
-User.prototype.setUsername = function(username) {
-	this.username = username;
-}
-User.prototype.getUsername = function() {
-	return this.username;
-}
-
-User.prototype.setEmail = function(email) {
-  this.email = email;
-};
-
-User.prototype.getEmail = function() {
-  return this.email;
-};
-
-//una sola función
-
-User.prototype.getP = function (attr) { return this[attr] };
-
-usuario.getP('email');
->> "andrearuizmendoza@gmail.com"
-
-User.prototype.setP = function (key, value) { this[key] = value; return this[key] };
-
-usuario.setP('username', 'andrearm10');
->> "andrearm10"
+User.prototype.setPropiedad = function(key, value) { this[key] = value; return this[key] };
 
 /*
 Crear un método “inspect” que liste todas las propiedades.
@@ -64,35 +27,32 @@ User.prototype.inspect = function(){
    var keys = [];
    for(var key in this){
       keys.push(key);
+      console.log(key + ':' + this[key]);
    }
    return keys;
 }
-
-usuario.inspect();
->> ["nombre", "apellidos", "username", "email", "getProperty", "getP", "setP", "inspect"]
 
 /*
 Crear un método “postMessage” que guarde un mensaje en la lista de mensajes y 
 devuelva el número de mensajes guardados en la lista. Los mensajes deben ser guardados en una variable privada.
 */
-var userModule = (function () {
+var User = function User(attributes) {
+  this.nombre = attributes.nombre;
+  this.apellidos = attributes.apellidos;
+  this.username = attributes.username;
+  this.email = attributes.email;
   var messages = [];
-  return {
-    postMessage : function(message) {
-      messages.push(message);
-      return messages.length;
-    }
+  this.postMessage = function(message) {
+    messages.push(message);
+    return messages.length;
   }
-}
-)();
->> undefined
-userModule.postMessage('mensaje 1');
->> 1
-userModule.postMessage('mensaje 2');
->> 2
+  this.countMessages = function() {
+    return messages.length;
+  }
+};
 
 /*
-⦁ Crear un módulo Users que permita:
+Crear un módulo Users que permita:
 ⦁ Registrar usuarios. Verificar que el objeto a agregar sea una instancia de la clase User.
 ⦁ Validar si el usuario o correo electrónico ya existe antes de agregar un usuario nuevo.
 ⦁ Devolver el número de usuarios registrados.
@@ -103,17 +63,29 @@ Debe crear un método auxiliar que devuelva el total de mensajes de cada usuario
 de cada usuario usando call o apply.
 */
 
-var UsersModule = (function () {
+var sumaTotal = function() {
+  var suma = 0;
+  for (var i = 0; i < arguments.length; i++) {
+    var usr = arguments[i]; 
+    suma = suma + usr.countMessages();
+  }
+  return suma;
+}
+
+var Users = (function () {
   var users = [];
   return {
     exists : function(user) {
-      if (users.indexOf(user.username) != -1 || users.indexOf(user.email) != -1) {
-        return true;
-      }
-      return false;
+      for (var i=0; i < users.length; i++) { 
+        var usr = users[i]; 
+        if (usr.nombre == user.nombre || usr.username == user.username) { 
+          return true; 
+        } 
+      } 
+      return false; 
     },
     addUser : function(user) {
-      if (user instanceOf User && !this.exists(user)) {
+      if (user instanceof User && !this.exists(user)) {
         users.push(user);
       }
       return users.length;
@@ -123,12 +95,16 @@ var UsersModule = (function () {
     },
     getProperty: function(property) {
       var properties = [];
-      for (var usr in users) {
+      for (var i=0; i < users.length; i++) {
+        var usr = users[i];
         if (usr.hasOwnProperty(property)) {
           properties.push(usr[property]);
         }
       }
       return properties;
+    },
+    totalMessages: function() {
+      return sumaTotal.apply(this, users);
     }
   }
 }
