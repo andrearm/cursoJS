c522bfc333dbd24eee3f9e2a41eadf6c7850bc5b
andrearm <andrearuizmendoza@gmail.com>
2013-11-07 17:00:28 -0500

    Update and rename segundaActividad.js.txt to segundaActividad.js

---
+++ SegundaActividad/segundaActividad.js
@@ -0,0 +1,6 @@
+/*
+1. Se debe crear un objeto que permita manipular elementos de una sección de una página web. 
+Debe soportar las siguientes operaciones:
+	a. Obtener elementos por id (un solo resultado) y por selector (varios resultados)
+	b. Agregar y eliminar elementos a la sección, con opción para definir los atributos de los elementos a agregar.
+*/
